eabcf037825d6b6b861095c4c696d930e49e2a8c
Leon <leonwheeler08@gmail.com>
2021-07-14 06:30:24 +0100

    Added HTML route for individual exercise and stats

--- src/routes/html/index.js
+++ src/routes/html/index.js
@@ -3,6 +3,18 @@ const path = require("path");
 
 const router = Router();
 
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../../src/public/exercise.html"));
+});
+
+router.get("/exercise/:id", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../../src/public/exercise.html"));
+});
+
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../../src/public/stats.html"));
+});
+
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../../../src/public/index.html"));
 });
